f85c1bc5b2f60141ad567f843bf3fbdb956a65f8
mystikshorror <mystikshorror@gmail.com>
2026-09-16 11:55:18 +0800

    Update markers in PPK main area and clean up unused lobby marker image

--- locations/ppk/ppk-f1-main-area.js
+++ locations/ppk/ppk-f1-main-area.js
@@ -25,7 +25,54 @@ export default [
         name: 'Go to Main Area 3'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-1-marker-1789530441841',
+        position: {
+          yaw: '40.10deg',
+          pitch: '-0.95deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_10.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_10.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_10.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_10.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-1-marker-1789530469519',
+        position: {
+          yaw: '13.30deg',
+          pitch: '-8.93deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/OUTSIDE/KOMTAR_outside_2.jpg',
+          originalUrl: './images/ppk/markers/OUTSIDE/KOMTAR_outside_2.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/OUTSIDE/KOMTAR_outside_2.jpg\'>\n        <img src=\'./images/ppk/markers/OUTSIDE/KOMTAR_outside_2.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -149,7 +196,31 @@ export default [
         name: 'Go to Main Area 7'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-5-marker-1789530520591',
+        position: {
+          yaw: '209.35deg',
+          pitch: '1.77deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_6.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_6.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_6.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_6.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -220,7 +291,31 @@ export default [
         name: 'Go to Main Area 9'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-7-marker-1789530554491',
+        position: {
+          yaw: '284.66deg',
+          pitch: '-1.89deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_11.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_11.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_11.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_11.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -264,7 +359,208 @@ export default [
         name: 'Go to Main Area 4'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530587069',
+        position: {
+          yaw: '166.17deg',
+          pitch: '-21.79deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_18.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_18.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_18.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_18.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530596679',
+        position: {
+          yaw: '41.02deg',
+          pitch: '-11.41deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_17.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_17.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_17.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_17.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530603885',
+        position: {
+          yaw: '131.72deg',
+          pitch: '22.99deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_14.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_14.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_14.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_14.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530610542',
+        position: {
+          yaw: '56.98deg',
+          pitch: '18.63deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_13.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_13.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_13.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_13.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530634384',
+        position: {
+          yaw: '31.95deg',
+          pitch: '-16.88deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530806311',
+        position: {
+          yaw: '132.32deg',
+          pitch: '-6.44deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Bahan Baru',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'info\' data-color=\'blue\'>\n        <h2>Bahan Baru</h2>\n        <p></p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530817793',
+        position: {
+          yaw: '132.24deg',
+          pitch: '-33.34deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Bahan Baru',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'info\' data-color=\'blue\'>\n        <h2>Bahan Baru</h2>\n        <p></p>\n      </custom-marker>'
+      },
+      
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530835054',
+        position: {
+          yaw: '85.63deg',
+          pitch: '-6.84deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Majalah',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'info\' data-color=\'blue\'>\n        <h2>Majalah</h2>\n        <p></p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-8-marker-1789530827677',
+        position: {
+          yaw: '84.02deg',
+          pitch: '-30.62deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Majalah',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'info\' data-color=\'blue\'>\n        <h2>Majalah</h2>\n        <p></p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -338,7 +634,54 @@ export default [
         name: 'Go to Main Area 12'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-10-marker-1789530758386',
+        position: {
+          yaw: '43.63deg',
+          pitch: '5.77deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_12.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_12.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_12.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_12.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-10-marker-1789530771002',
+        position: {
+          yaw: '352.20deg',
+          pitch: '-27.92deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_7.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_7.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_7.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_7.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -361,7 +704,31 @@ export default [
         name: 'Go to Main Area 10'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-11-marker-1789530747814',
+        position: {
+          yaw: '225.88deg',
+          pitch: '-33.35deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_15.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_15.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_15.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_15.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
@@ -389,7 +756,96 @@ export default [
         name: 'Go to Main Area 10'
       },
     ],
-    markers: [],
+    markers: [
+      {
+        id: 'ppk-f1-main-area-12-marker-1789530644268',
+        position: {
+          yaw: '144.79deg',
+          pitch: '-57.18deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_5.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-12-marker-1789530652892',
+        position: {
+          yaw: '64.33deg',
+          pitch: '-16.06deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'image',
+          title: '',
+          icon: 'info',
+          color: 'blue',
+          animated: false,
+          imageSrc: './images/ppk/markers/LOBBY/KOMTAR_lobby_16.jpg',
+          originalUrl: './images/ppk/markers/LOBBY/KOMTAR_lobby_16.jpg',
+          caption: ''
+        },
+        html: '<custom-marker type=\'image\' data-icon=\'info\' data-color=\'blue\' data-url=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_16.jpg\'>\n        <img src=\'./images/ppk/markers/LOBBY/KOMTAR_lobby_16.jpg\' alt=\'\' style=\'cursor:pointer;\' />\n        <h2></h2>\n        <p style=\'font-size: 11px; opacity:0.7;\'>🔍 Click to enlarge</p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-12-marker-1789530695401',
+        position: {
+          yaw: '345.28deg',
+          pitch: '-9.74deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Staff Only',
+          icon: 'no-entry',
+          color: 'red',
+          animated: true,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'no-entry\' data-color=\'red\' animated>\n        <h2>Staff Only</h2>\n        <p></p>\n      </custom-marker>'
+      },
+      {
+        id: 'ppk-f1-main-area-12-marker-1789530714963',
+        position: {
+          yaw: '31.25deg',
+          pitch: '-8.81deg'
+        },
+        size: {
+          width: 44,
+          height: 44
+        },
+        anchor: 'center center',
+        data: {
+          type: 'info',
+          title: 'Staff Only',
+          icon: 'no-entry',
+          color: 'red',
+          animated: true,
+          content: ''
+        },
+        html: '<custom-marker type=\'info\' data-icon=\'no-entry\' data-color=\'red\' animated>\n        <h2>Staff Only</h2>\n        <p></p>\n      </custom-marker>'
+      }
+    ],
     data: {
       floor: 'f1',
       tags: [
